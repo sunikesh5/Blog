e5029471cc22b989a1b674580162b009faa0c77a
sunikesh shukla <sunikesh.shukla5@gmail.com>
2024-05-27 15:12:16 -0500

    Delete blog_site directory

--- blog_site/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home_page, name="home-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="postdetail-page")
-]
